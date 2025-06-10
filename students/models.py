53ac9cabb8a2c62d4aa768542aaf3b10d91454e8
Romaxa-FS <morramzes@gmail.com>
2025-06-11 00:16:27 +0700

    Точно добавлены модели и пути url

--- students/models.py
+++ students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    value = models.IntegerField()
---
+++ students/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('student/add/', views.student, name='add_student'),
+    path('student/edit/<int:pk>/', views.edit_student, name='edit_student'),
+    path('student/delete/<int:pk>/', views.delete_student, name='delete_student'),
+    path('subject/add/', views.add_subject, name='add_subject'),
+    path('subject/delete/<int:pk>/', views.delete_subject, name='delete_subject'),
+    path('grade/add/', views.add_grade, name='add_grade'),
+    path('grade/delete/<int:pk>/', views.delete_grade, name='delete_grade'),
+    path('report/', views.report, name='report'),
+]
